b713aed638954bde18c697e8cfaf00d047785d74
Olof Kindgren <olof.kindgren@gmail.com>
2022-05-12 13:33:31 +0200

    wip

--- data/params.tcl
+++ data/params.tcl
@@ -3,4 +3,7 @@ set ::env(CLOCK_PORT) "i_clk"
 set ::env(DESIGN_IS_CORE) 0
 
 set ::env(FP_SIZING) "absolute"
-set ::env(DIE_AREA) "0 0 200 200"
+set ::env(DIE_AREA) "0 0 177.5 177.5"
+#nok set ::env(DIE_AREA) "0 0 177.2 177.2"
+#okset ::env(DIE_AREA) "0 0 177.5 177.5"
+#set ::env(PL_TARGET_DENSITY) 0.6
