978a4a35ef1853eac38abbc0c69a5e2ac7d8a290
Olof Kindgren <olof.kindgren@gmail.com>
2022-10-30 12:26:53 +0100

    Tie off extension interface

--- rtl/subservient_core.v
+++ rtl/subservient_core.v
@@ -282,6 +282,14 @@ module subservient_core
       .o_dbus_we   (wb_dbus_we),
       .o_dbus_cyc  (wb_dbus_stb),
       .i_dbus_rdt  (wb_dbus_rdt),
-      .i_dbus_ack  (wb_dbus_ack));
+      .i_dbus_ack  (wb_dbus_ack),
+
+      //Extension interface
+      .o_ext_funct3 (),
+      .i_ext_ready  (1'b0),
+      .i_ext_rd     (32'd0),
+      .o_ext_rs1    (),
+      .o_ext_rs2    (),
+      .o_mdu_valid  ());
 
 endmodule
